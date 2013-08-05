4a76a6f2770f223540432925ad43c96dc9ae7a03
Darrel <darrel@tavis.ca>
2013-08-04 22:04:19 -0400

    Initial commit

---
+++ src/HomeDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tavis.Home
+{
+    public class HomeDocument   
+    {
+
+    }
+
+    public class ResourceLink : Link
+    {
+        
+    }
+}
